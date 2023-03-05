c9e6d49d33d722ac8f22a728a3b515a7ad035cb3
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-05 17:24:36 +0100

    [generics, status] and (Response) was all imported from -> {rest_framework}.

---
+++ authentication/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+
+# Create your views here.
+
+
+class HelloAuthView(generics.GenericAPIView):
+  def get(self, request):
+      return(data={"message": "How are you?"},
+       status = status.HTTP_200_OK
+      )
