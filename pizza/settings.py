6780daf8a6a5a53558554e3e428bd8f57688a1ce
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-05 19:06:12 +0100

    Add authentication user model reference to settings
    The commit adds the reference to the custom user model defined in the authentication app to the AUTH_USER_MODEL setting in Django settings module. This is necessary to tell Django to use the custom user model instead of the default one provided by Django.

--- pizza/settings.py
+++ pizza/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'phonenumber_field',
 ]
 
+
+AUTH_USER_MODEL = 'authentication.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
