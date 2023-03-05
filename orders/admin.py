ee0cc125d5e4ed12b8c20634ff64f5faefacdb43
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-05 20:45:51 +0100

    comments was added .

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Order
 
+# * Registering the Order model with the admin site.
+
 
 @admin.register(Order)
+# * Defining an admin class for the Order model.
 class OrderAdmin(admin.ModelAdmin):
+    # *  Specifying the fields to be displayed in the admin interface for the Order model.
     list_display = ['size', 'order_status', 'quantity', 'created_at']
     list_filter = ['created_at', 'order_status', 'size']
