a954decb0fb8b08bc1fbf65371191a1fcd69c3d0
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-05 17:10:17 +0100

    orders (app) was added.

--- pizza/settings.py
+++ pizza/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'authentication.apps.AuthenticationConfig',
+    'orders.apps.OrdersConfig'
 ]
 
 MIDDLEWARE = [
