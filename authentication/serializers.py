b92c64d801478f3c9cdb13ca8a0f1047a79f352e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-07 06:56:27 +0100

    [PhoneNumberField] was imported with (username, email, phone_number) added as the {UserCreationSerializer} class fields.

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,2 +1,9 @@
 from .models import User
 from rest_framework import serializers
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=30, unique=True)
+    email = serializers.EmailField(max_length=90, unique=True)
+    phone_number = PhoneNumberField(null=False, unique=True)
