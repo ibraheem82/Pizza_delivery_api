379ed3707c066fa12e410fe8d1facd943fb59379
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-09 01:03:01 +0100

    Added validation logic to ensure that no two users can have the same username, email, or phone number in the "User" model.
    Commit message: "Added validation to User model fields in Meta class and validate() method"

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,9 +1,39 @@
 from .models import User
 from rest_framework import serializers
-from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.serializerfields import PhoneNumberField
 
 
 class UserCreationSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=30, unique=True)
-    email = serializers.EmailField(max_length=90, unique=True)
-    phone_number = PhoneNumberField(null=False, unique=True)
+    username = serializers.CharField(max_length=30)
+    email = serializers.EmailField(max_length=90)
+    phone_number = PhoneNumberField(allow_null=False, allow_blank=False)
+    password = serializers.CharField(min_length=7)
+
+
+# TODO: start writing commits from here.
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'password']
+
+    def validate(self, attrs):
+        username_exists = User.objects.filter(
+            username=attrs['username']).exists()
+        if username_exists:
+            raise serializers.ValidationError(
+                detail="User with username exists ")
+
+        email_exists = User.objects.filter(
+            username=attrs['email']).exists()
+        if email_exists:
+            raise serializers.ValidationError(
+                detail="User with email exists ")
+
+        phonenumber_exists = User.objects.filter(
+            username=attrs['phone_number']).exists()
+        if phonenumber_exists:
+            raise serializers.ValidationError(
+                detail="User with phonenumber exists ")
+
+        return super().validate(attrs)
