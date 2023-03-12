5facd8111658d34607e4ac97383560acb769603f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-12 16:08:31 +0100

    field updated ...

--- orders/admin.py
+++ orders/admin.py
@@ -8,6 +8,6 @@ from .models import Order
 # * Defining an admin class for the Order model.
 class OrderAdmin(admin.ModelAdmin):
     # *  Specifying the fields to be displayed in the admin interface for the Order model.
-    list_display = ['size', 'order_status', 'quantity', 'created_at']
+    list_display = ['size', 'order_status', 'quantity', 'date_purchased']
     # * Adding filters to the admin interface for the Order model based on the specified fields.
-    list_filter = ['created_at', 'order_status', 'size']
+    list_filter = ['date_purchased', 'order_status', 'size']
