6176c39c953dcb40de7f05cf166e6ad15a86150c
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-12 15:51:06 +0100

    Add created_at field to OrderCreationSerializer with a default value of the current date and time using serializers.CreateOnlyDefault.
    Commit message: "Add created_at field to OrderCreationSerializer with default value using CreateOnlyDefault."

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,9 +6,11 @@ class OrderCreationSerializer(serializers.ModelSerializer):
   size = serializers.CharField(max_length=25)
   order_status = serializers.HiddenField(default='PENDING')
   quantity = serializers.IntegerField()
-  created_at = serializers.DateTimeField(auto_now_add=True)
-    class Meta:
-      model = Order
+  created_at = serializers.DateTimeField(default=serializers.CreateOnlyDefault(datetime.datetime.now))
+
+
+  class Meta:
+    model = Order
       fields = ['size', 'order_status', 'quantity']
       
     
