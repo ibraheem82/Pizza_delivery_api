7a892ea0e7f836e025739d5f9142fa9d1a351254
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-12 15:25:03 +0100

    Created a new serializer called OrderCreationSerializer that inherits from serializers.ModelSerializer and defines fields for the size, order_status, quantity, and created_at attributes of an Order instance.
    Commit message: "Created OrderCreationSerializer to define fields for creating a new Order instance."

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,14 @@
 from .models import Order
 from rest_framework import serializers
 
-
+class OrderCreationSerializer(serializers.ModelSerializer):
+  size = serializers.CharField(max_length=25)
+  order_status = serializers.HiddenField(max_length=25, default=PENDING)
+  quantity = serializers.IntegerField()
+  created_at = serializers.DateTimeField(auto_now_add=True)
+    class Meta:
+      model = Order
+      fields = ['size', 'order_status', 'quantity']
       
     
 
