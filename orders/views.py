280980a3d59e5653fcae95cbf0b7cc29eeb462a9
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-14 16:58:55 +0100

    Added a new API endpoint for retrieving a list of all orders using the OrderCreationSerializer. The queryset is set to retrieve all Order objects. The HTTP method used is GET. Inside the get method, all orders are retrieved and then serialized using the serializer class. Finally, the serialized data is returned in a HTTP response with status 200 OK.

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
-
+from . import serializers
+from .models import Order
 
 # Create your views here.
 
 
 class HelloOrderView(generics.GenericAPIView):
+    def get(self, request):
+        return Response(data={"message":"Orders"},status=status.HTTP_200_OK)
+
+
+
+class OrderCreateListView(generics.GenericAPIView):
+  serializer_class = serializers.OrderCreationSerializer
+  queryset = Order.objects.all()
   def get(self, request):
-      return Response(data={"message":"Orders"},status=status.HTTP_200_OK)
+    orders = Order.objects.all()
+    serializer = self.serializer_class(instance = orders, many = True)
+
+    return Response(data = serializer.data, status = status.HTTP_200_OK)
+
+    def post(self, request):
+      pass
+
+
+
+class OrderDetailView(generics.GenericAPIView):
+    def get(self, request, order_id):
+      pass
+
+    def put(self, request, order_id):
+      pass
+
+
+    def delete(self, request, order_id):
+      pass
+
+      
