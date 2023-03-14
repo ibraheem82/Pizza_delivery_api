1ac7c1c4efa44e43798409e3b653f04a4a6db99c
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-14 17:11:12 +0100

    Imported the IsAuthenticated permission class from the rest_framework.permissions module.

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from . import serializers
 from .models import Order
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
